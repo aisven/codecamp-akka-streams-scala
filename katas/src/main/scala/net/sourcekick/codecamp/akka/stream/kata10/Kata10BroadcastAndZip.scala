1db73f5fa5e8e250a6ca8996efafb1aa770978fb
sourcekick <sourcekick@gmail.com>
2017-02-12 18:31:39 +0100

    kata10

---
+++ katas/src/main/scala/net/sourcekick/codecamp/akka/stream/kata10/Kata10BroadcastAndZip.scala
@@ -0,0 +1,60 @@
+package net.sourcekick.codecamp.akka.stream.kata10
+
+import akka.NotUsed
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.{ClosedShape, Graph}
+
+import scala.collection.immutable.Seq
+import scala.concurrent.Future
+
+object Kata10BroadcastAndZip {
+
+  /**
+    * Task: Create an akka.stream.Graph that has no unconnected inlets or outlets
+    * anywhere and that looks like the following:
+    * <ul>
+    * <li>It starts with source createSource()</li>.
+    * <li>It forks via a broadcasting uniform fan out into three branches.</li>.
+    * <li>Branch 0 goes via createFlow0_0()
+    * and then createFlow0_1().</li>.
+    * <li>Branch 1 just goes via createFlow1_0().</li>.
+    * <li>Branch 2 goes via createFlow2_0()
+    * and createFlow2_1()
+    * and then createFlow2_2().</li>.
+    * <li>It joins the three branches again via a zipping fan in
+    * that simply uses addition to combine each three integers by calculating their sum.</li>.
+    * <li>The outlet of the fan in is connected to the sink createSink().</li>.
+    * </ul>
+    * </ul>
+    * To define processing graphs with patterns such as forks, fan out, balancing,
+    * joins or fan in it is necessary to work with the abstractions Shape or AbstractShape
+    * and their sub-classes in order to create an akka.stream.Graph.
+    * For more information see the reference documentation linked below.
+    * <p/>
+    * Reference: http://doc.akka.io/docs/akka/current/java/stream/stream-graphs.html
+    * <p/>
+    * Check: The kata is solved when the corresponding unit test is green.
+    *
+    * @return The graph.
+    */
+  private[kata10] def createGraphWithFanInAndFanOut: Graph[ClosedShape.type, Future[Seq[Int]]] = {
+    ???
+  }
+
+  private def createSource: Source[Int, NotUsed] = Source(1 to 10)
+
+  private def createFlow0_0: Flow[Int, Int, NotUsed] = Flow.fromFunction[Int, Int](i => i - 1)
+
+  private def createFlow0_1: Flow[Int, Int, NotUsed] = Flow.fromFunction[Int, Int](i => i - 2)
+
+  private def createFlow1_0: Flow[Int, Int, NotUsed] = Flow.fromFunction[Int, Int](i => i * 2)
+
+  private def createFlow2_0: Flow[Int, Int, NotUsed] = Flow.fromFunction[Int, Int](i => i + 1)
+
+  private def createFlow2_1: Flow[Int, Int, NotUsed] = Flow.fromFunction[Int, Int](i => i + 2)
+
+  private def createFlow2_2: Flow[Int, Int, NotUsed] = Flow.fromFunction[Int, Int](i => i + 3)
+
+  private def createSink: Sink[Int, Future[Seq[Int]]] = Sink.seq[Int]
+
+}
---
+++ katas/src/test/scala/net/sourcekick/codecamp/akka/stream/kata10/Kata10BroadbastAndZipSpec.scala
@@ -0,0 +1,71 @@
+package net.sourcekick.codecamp.akka.stream.kata10
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.RunnableGraph
+import org.scalatest.{AsyncWordSpec, Matchers, ParallelTestExecution}
+
+class Kata10BroadbastAndZipSpec extends AsyncWordSpec with Matchers with ParallelTestExecution {
+
+  /*
+   * Please do not read this test code while solving any of the katas! Spoiler warning!
+   *
+   * Just run it with right-click on the test class name above.
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   */
+
+  "Kata10BroadbastAndZip" must {
+
+    "create the correct graph" in {
+
+      implicit val system = ActorSystem("Kata10")
+      implicit val materializer = ActorMaterializer()
+
+      val g = Kata10BroadcastAndZip.createGraphWithFanInAndFanOut
+
+      val rg = RunnableGraph.fromGraph(g)
+
+      rg.run()
+        .map(integers => {
+          integers shouldBe Seq(7, 11, 15, 19, 23, 27, 31, 35, 39, 43)
+        })
+    }
+
+  }
+
+}
