7372109725d5fee041e1048a248ded431604e14c
sourcekick <sourcekick@gmail.com>
2017-02-12 23:11:36 +0100

    kata13

---
+++ katas/src/main/scala/net/sourcekick/codecamp/akka/stream/kata13/Kata13RollingAverage.scala
@@ -0,0 +1,24 @@
+package net.sourcekick.codecamp.akka.stream.kata13
+
+import akka.NotUsed
+import akka.stream.scaladsl.Flow
+
+private[kata13] object Kata13RollingAverage {
+
+  /**
+    * Task: Create a akka.stream.scaladsl.Flow that calculates the rolling average
+    * over the incoming sequence of Float values
+    * with sliding window semantics
+    * where each window contains 100 values
+    * and where the following window does not contain the first 10 values of the previous window
+    * and where the following window contains also the 10 first values of the window thereafter.
+    * <p/>
+    * Check: The kata is solved when the corresponding unit test is green.
+    *
+    * @return The flow.
+    */
+  private[kata13] def createRollingAverageFlow(): Flow[Float, Float, NotUsed] = {
+    ???
+  }
+
+}
---
+++ katas/src/test/scala/net/sourcekick/codecamp/akka/stream/kata13/Kata13RollingAverageSpec.scala
@@ -0,0 +1,126 @@
+package net.sourcekick.codecamp.akka.stream.kata13
+
+import java.nio.file.Paths
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{FileIO, Flow, Framing, Keep, Sink, Source}
+import akka.stream.{ActorMaterializer, IOResult}
+import akka.util.ByteString
+import org.scalatest.{AsyncWordSpec, Matchers, ParallelTestExecution}
+
+import scala.concurrent.Future
+import scala.util.Random
+
+class Kata13RollingAverageSpec extends AsyncWordSpec with Matchers with ParallelTestExecution {
+
+  /*
+   * Please do not read this test code while solving any of the katas! Spoiler warning!
+   *
+   * Just run it with right-click on the test class name above.
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   *
+   */
+
+  "Kata13RollingAverage" must {
+
+    "calculate the correct rolling average" in {
+
+      implicit val system = ActorSystem("Kata13")
+      implicit val materializer = ActorMaterializer()
+
+      val floatSource = createFile2FloatSource("kata13.txt")
+      //val floatSource = createFile2FloatSource("1to1000.txt")
+
+      val flow = Kata13RollingAverage.createRollingAverageFlow()
+
+      val runnableGraph = floatSource.viaMat(flow)(Keep.left).toMat(Sink.seq)(Keep.both)
+
+      val tuple = runnableGraph.run()
+
+      val ioResultFuture = tuple._1
+      val resultFuture = tuple._2
+
+      val expectedFuture = createFile2FloatSource("kata13expected.txt").runWith(Sink.seq)
+
+      expectedFuture
+        .map(expectedAverages => {
+          expectedAverages.size shouldBe 91
+        })
+        .flatMap(_ => ioResultFuture)
+        .map(ioResult => {
+          ioResult.wasSuccessful shouldBe true
+        })
+        .flatMap(_ => resultFuture)
+        .map(averages => {
+          //averages.foreach(println)
+          averages.size shouldBe expectedFuture.value.get.toOption.get.size
+          averages shouldBe expectedFuture.value.get.toOption.get
+        })
+    }
+
+    "generate new random float sequence" in {
+
+      val r = Random
+      Future {
+        (1 to 1000)
+          .map(_ => r.nextFloat() * 100F)
+          .map(f => f % 4)
+          .map(f => f + 19F)
+          .map(f => "%.2f".format(f).toFloat)
+          .sorted
+      }.map(floats => {
+        //floats.foreach(println)
+        floats.size shouldBe 1000
+      })
+    }
+
+  }
+
+  private def createFile2FloatSource(fileName: String): Source[Float, Future[IOResult]] = {
+    val url = Thread.currentThread().getContextClassLoader.getResource(fileName)
+
+    val lineSource: Source[String, Future[IOResult]] = FileIO
+      .fromPath(Paths.get(url.getPath))
+      .viaMat(Framing.delimiter(ByteString("\n"), 1000, allowTruncation = true))(Keep.left)
+      .viaMat(Flow.fromFunction(bs => bs.utf8String))(Keep.left)
+
+    val floatSource = lineSource.viaMat(Flow.fromFunction(s => s.toFloat))(Keep.left)
+
+    floatSource
+  }
+
+}
